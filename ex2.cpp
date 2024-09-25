c0244d3e6f2e5093785c0dccc3d8d516876f29b1
Youssef Bakouny <Youssef.Bakouny@usj.edu.lb>
2024-09-25 11:55:18 +0300

    Implement Ex2 Partie 5: refactored code

--- ex2.cpp
+++ ex2.cpp
@@ -1,39 +1,30 @@
 #include <iostream>
-using namespace std; 
+using namespace std;
 
-int main() {
-    cout << "Entrez l'entier pour lequel vous vouliez afficher la table de multiplication:" << endl;
-    int n;
+int saisir(int inf, int sup) {
+    int res;
     bool borneInf;
     bool borneSup;
     do {
-        cout << "Entrez un entier positif inférieur à 70: ";
-        cin >> n;
-        borneInf = n <= 0;
-        borneSup = n >= 70; 
+        cout << "Entrez un entier superieur a "<<inf<<" et inférieur à "<<sup<<": ";
+        cin >> res;
+        borneInf = res <= inf;
+        borneSup = res >= sup; 
         if (borneInf) {
-            cout << "Votre nombre est inférieur ou égual à 0 !" << endl;
+            cout << "Votre nombre est inférieur ou égual à "<<inf<<" !" << endl;
         }
         if (borneSup) {
-            cout << "Votre nombre est supérieur ou égual à 70 !" << endl;
+            cout << "Votre nombre est supérieur ou égual à "<<sup<<" !" << endl;
         }
     } while (borneInf || borneSup);
-    
-    cout << "Entrez la borne supérieur de cette table de multiplication:" << endl;
-    int tableBorneSup;
-    do {
-        cout << "Entrez un entier superieur a 8 et inférieur à 30: ";
-        cin >> tableBorneSup;
-        borneInf = tableBorneSup <= 8;
-        borneSup = tableBorneSup >= 30; 
-        if (borneInf) {
-            cout << "Votre nombre est inférieur ou égual à 8 !" << endl;
-        }
-        if (borneSup) {
-            cout << "Votre nombre est supérieur ou égual à 30 !" << endl;
-        }
-    } while (borneInf || borneSup);
+    return res;
+}
 
+int main() {
+    cout << "Entrez l'entier pour lequel vous vouliez afficher la table de multiplication:" << endl;
+    int n = saisir(0,70);
+    cout << "Entrez la borne supérieur de cette table de multiplication:" << endl;
+    int tableBorneSup = saisir(8,30);
 
     cout << "Table de multiplication de: " << n << endl;
     for (int i = tableBorneSup; i >= 0; i--)
