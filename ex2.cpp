c62c48f4f3991581c711edea593f7c81cf800bec
Youssef Bakouny <Youssef.Bakouny@usj.edu.lb>
2024-09-25 10:43:31 +0300

    Implement Ex2 Partie 1

--- ex2.cpp
+++ ex2.cpp
@@ -2,7 +2,15 @@
 using namespace std; 
 
 int main() {
-    cout << "Ex2" << endl;
+    cout << "Entrez un entier positif: ";
+    int n;
+    cin >> n;
+    cout << "Table de multiplication de: " << n << endl;
+    for (int i = 10; i >= 0; i--)
+    {
+        cout << n << " x " << i << " = " << n * i << endl;
+    }
+    
     return 0;
 
 }
