15f933b15281ebf7112a1c10a7a3a6157a9aa13c
Youssef Bakouny <Youssef.Bakouny@usj.edu.lb>
2024-09-25 11:46:27 +0300

    Implement Ex2 Partie 5: unrefactored code

--- ex2.cpp
+++ ex2.cpp
@@ -2,6 +2,7 @@
 using namespace std; 
 
 int main() {
+    cout << "Entrez l'entier pour lequel vous vouliez afficher la table de multiplication:" << endl;
     int n;
     bool borneInf;
     bool borneSup;
@@ -18,8 +19,24 @@ int main() {
         }
     } while (borneInf || borneSup);
     
+    cout << "Entrez la borne supérieur de cette table de multiplication:" << endl;
+    int tableBorneSup;
+    do {
+        cout << "Entrez un entier superieur a 8 et inférieur à 30: ";
+        cin >> tableBorneSup;
+        borneInf = tableBorneSup <= 8;
+        borneSup = tableBorneSup >= 30; 
+        if (borneInf) {
+            cout << "Votre nombre est inférieur ou égual à 8 !" << endl;
+        }
+        if (borneSup) {
+            cout << "Votre nombre est supérieur ou égual à 30 !" << endl;
+        }
+    } while (borneInf || borneSup);
+
+
     cout << "Table de multiplication de: " << n << endl;
-    for (int i = 10; i >= 0; i--)
+    for (int i = tableBorneSup; i >= 0; i--)
     {
         cout << n << " x " << i << " = " << n * i << endl;
     }
