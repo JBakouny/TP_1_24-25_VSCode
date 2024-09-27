fcf128cd4d0bf9a0dd6ce617c5b9738344c58437
Youssef Bakouny <Youssef.Bakouny@usj.edu.lb>
2024-09-27 14:05:37 +0300

    Ex3: refactor to create a distance function

--- ex3.cpp
+++ ex3.cpp
@@ -11,10 +11,13 @@ void saisirPoint(const string & nom, int &x, int &y) {
     cin >> y;
 }
 
+double distance(int xa, int ya, int xb, int yb) {
+    return sqrt(pow((xb-xa), 2) + pow((yb-ya), 2));
+}
+
 int main() {
     int xa, ya, xb, yb;
     saisirPoint("A", xa, ya);
     saisirPoint("B", xb, yb);
-    cout << "AB = " << sqrt(pow((xb-xa), 2) + pow((yb-ya), 2)) << endl;
-
+    cout << "AB = " << distance(xa, ya, xb, yb) << endl;
 }
